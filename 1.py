3feede17d78b971586da7044b44dae48fa784526
Alisha21 <53305180+Alisha21@users.noreply.github.com>
2020-06-25 16:47:50 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -13,14 +13,14 @@ class agentselector():
         
     def agentselector(self,roleofissue):
         list = []
-        if self.selectmode == "All Available":
+        if self.selectmode.lower() == "all available":
             for em in self.listofagents:
                 for s in em.roles:
                     if s == roleofissue and em.is_available == "True":
                         list.append(em.name)
                         break
                     
-        elif self.selectmode == "Least Busy":
+        elif self.selectmode.lower() == "least busy":
             n = []
             for em in self.listofagents:
                   n.append(em.available_since)
@@ -34,7 +34,7 @@ class agentselector():
                             break
                   
               
-        elif self.selectmode == "Random":
+        elif self.selectmode.lower() == "random":
             for em in self.listofagents:
                 for s in em.roles:
                     if s == roleofissue:
@@ -47,19 +47,21 @@ ls = []
 noofagents = int(input("Please enter the no. of agents: "))
 for i in range(noofagents):
     name = input("Enter the name of the agent: ")
-    a = input("Enter the agent is available or not? ")
-    b = input("The agent is available since? (Enter the time in hh:mm:ss format): ")
-    b.split(":")
-    h = b[0]
-    m = b[1]
-    s = b[2]
-    timeinmin = h * 60 + m
-    timeinsec = timeinmin * 60 + s 
+    a = input("Enter the agent is available or not? (True or False) ")
+    if  a.capitalize() == "True":
+        b = input("The agent is available since? (Enter the time in hh:mm:ss format): ").split(":")
+        h = int(b[0])
+        m = int(b[1])
+        s = int(b[2])
+        timeinmin = h * 60 + m
+        timeinsec = timeinmin * 60 + s
+    else:
+        timeinsec = 0 
     c = input("Enter the list of roles that the agent is capable of separated by comma: ").split(",")
-    ls.append(agent(name,a,timeinsec,c))
+    ls.append(agent(name,a.capitalize(),timeinsec,c))
 
 selectmode = input("Enter the selection mode: ")
-role = input("Enter role of issue: ")
+role = input("Enter role of issue that you want to present to any agent: ")
 obj = agentselector(ls,selectmode)
 print(obj.agentselector(role))    
 
